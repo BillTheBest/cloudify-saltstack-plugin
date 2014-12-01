bac62793fe598f8ee6160bb8611ff9ceac1c69db
Michał Soczewka <michal.soczewka@codilime.com>
2014-12-01 13:52:15 +0100

    CFY-1708 Cleanup: changes in plugin-serving script
    - serving plugin stuff from a temporary directory
    - support for bash < 4.3

--- tests/system_test/serve_plugin_on_http.sh
+++ tests/system_test/serve_plugin_on_http.sh
@@ -3,7 +3,7 @@
 
 SOURCE_DIR=../../
 SOURCE_YAML=plugin.yaml
-TARGET_DIR=temp_dir
+TARGET_DIR="/tmp/temp-saltstack-plugin-$$"
 TARGET_YAML=plugin.yaml
 TARGET_ZIP=plugin.zip
 PORT=8001
@@ -27,9 +27,17 @@ function _cleanup {
     done
     set -x
     rm -v "${target_path}/${TARGET_YAML}" "${target_path}/${TARGET_ZIP}"
-    rmdir -v "${target_path}"
+    rmdir -vp "${target_path}" 2>/dev/null
     _pids=()
 }
+
+
+netstat=`netstat -4tlNnp 2>/dev/null | grep -E "^tcp([[:space:]]+[[:digit:]]+){2}[[:space:]]([[:digit:]]+\.){3}[[:digit:]]:${PORT}.*LISTEN"`
+[ -n "${netstat}" ] && {
+    pids=`echo "$netstat" | awk '{print $7}' | cut -d/ -f1 | xargs`
+    kill ${pids}
+}
+
 
 for s in HUP INT QUIT TERM; do
     trap '_cleanup' "${s}"
@@ -41,16 +49,30 @@ set -e
 
 
 mkdir -p "${TARGET_DIR}"
-target_path="`pwd`/${TARGET_DIR}"
+if [ "${TARGET_DIR:0:1}" = / ]; then
+    target_path="${TARGET_DIR}"
+else
+    target_path="`pwd`/${TARGET_DIR}"
+fi
 
 pushd "${SOURCE_DIR}"
 cp -v "${SOURCE_YAML}" "${target_path}/${TARGET_YAML}"
-rm -v "${target_path}/${TARGET_ZIP}" || true
 zip -r "${target_path}/${TARGET_ZIP}" *
 popd
 
 pushd "${TARGET_DIR}"
 python -m SimpleHTTPServer "${PORT}" & _pids=(${_pids[@]} $!)
-sleep "${TIMEOUT}" & _pids=(${_pids[@]} $!)
-wait -n
-_cleanup
+
+# Support for older bash, sorry.
+if [ ${BASH_VERSINFO[0]} -gt 4 \
+        -o ${BASH_VERSINFO[0]} -eq 4 -a ${BASH_VERSINFO[1]} -ge 3 ]; then
+    sleep "${TIMEOUT}" & _pids=(${_pids[@]} $!)
+    wait -n && _cleanup
+else
+    set -x
+    for (( i = 0; i < ${TIMEOUT}; ++i )); do
+        sleep 1 || break
+    done
+    set +x
+    [ ${i} -ge ${TIMEOUT} ] && _cleanup
+fi
